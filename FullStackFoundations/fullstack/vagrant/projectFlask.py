2c27c9cd8b8537301642c6bafcc6b924171de4e4
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2015-09-28 03:39:25 -0700

    Adding delete menu item

--- FullStackFoundations/fullstack/vagrant/projectFlask.py
+++ FullStackFoundations/fullstack/vagrant/projectFlask.py
@@ -51,9 +51,18 @@ def editMenuItem(restaurant_id, menu_id):
             'editmenuitem.html', restaurant_id=restaurant_id, i=editedItem, item=editedItem)
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/',methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    deleteItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(deleteItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'deletemenuitem.html', restaurant_id=restaurant_id,  item=deleteItem)
 
 if __name__ == '__main__':
     app.debug = True
