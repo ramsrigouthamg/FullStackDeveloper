863b45f585433927a9d1dd4ea3a90f26d9989474
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2015-09-28 03:51:24 -0700

    Adding flashing when item is deleted , edited or added.

--- FullStackFoundations/fullstack/vagrant/projectFlask.py
+++ FullStackFoundations/fullstack/vagrant/projectFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template,request, redirect,url_for
+from flask import Flask , render_template,request, redirect,url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -28,6 +28,7 @@ def newMenuItem(restaurant_id):
             name=request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -43,6 +44,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash("Menu item has been edited!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
@@ -57,6 +59,7 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(deleteItem)
         session.commit()
+        flash("Menu item has been deleted!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
@@ -65,5 +68,6 @@ def deleteMenuItem(restaurant_id, menu_id):
             'deletemenuitem.html', restaurant_id=restaurant_id,  item=deleteItem)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
