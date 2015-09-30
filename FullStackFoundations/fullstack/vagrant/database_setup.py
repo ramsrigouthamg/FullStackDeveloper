be226241fa7fd6728e675b7827685c0f96fbfb7e
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2015-09-29 22:01:45 -0700

    Adding JSON API Support to return Menu items as JSON list.

--- FullStackFoundations/fullstack/vagrant/database_setup.py
+++ FullStackFoundations/fullstack/vagrant/database_setup.py
@@ -25,6 +25,20 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    
+# We added this serialize function to be able to send JSON objects in a
+# serializable format
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
