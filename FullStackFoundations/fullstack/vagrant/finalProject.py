098cf2bdf8645d539554b1ffe0c7617b16b761f8
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2015-09-30 22:21:13 -0700

    Final Project - Commit  after Mockup and Routing.

---
+++ FullStackFoundations/fullstack/vagrant/finalProject.py
@@ -0,0 +1,43 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/restaurants/')
+def showRestaurants():
+	return "Thsi page will show all of my restaurants"
+ 
+
+@app.route('/restaurant/new/')
+def newRestaurant():
+	return "This page will be for making a new restaurant"
+
+@app.route('/restaurant/<int:restaurant_id>/edit/')
+def editRestaurant(restaurant_id):
+	return "This page will be for editing restaurant: %s"%restaurant_id
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete/')
+def deleteRestaurant(restaurant_id):
+	return "This page will be for deleting restaurant: %s"%restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+def showMenu(restaurant_id):
+	return "This page is the menu for restaurant : %s"%restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new/')
+def newmMenuitem(restaurant_id):
+	return "This page is for making a new menu item for restaurant : %s"%restaurant_id
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def editMenuitem(restaurant_id,menu_id):
+	return "This page is for editing menu item : %s"%menu_id
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def deleteMenuitem(restaurant_id,menu_id):
+	return "This page is for editing menu item : %s"%menu_id
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
